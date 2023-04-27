f54dfecd647c1a59ae1c512ba097c5880f656e60
Nicole Gottschall <ngottschall@web.de>
2023-04-27 09:44:06 +0200

    adds database in form of JSON-Object

---
+++ usersDb.js
@@ -0,0 +1,24 @@
+var usersData = [
+    {
+        id: 1,
+        name: "Thomas",
+        age: 30
+    },
+    {
+        id: 2,
+        name: 'Max',
+        age: 24
+    },
+    {
+        id: 3,
+        name: 'Vanessa',
+        age: 28
+    },
+    {
+        id: 4,
+        name: 'Hannah',
+        age: 21
+    }
+]
+
+module.exports = usersData; //provide data to other modules
